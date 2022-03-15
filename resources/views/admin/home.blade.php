b51608a617f617497bf6464c4bbae795343c23c2
stfDeveloper <sstefancosmin@icloud.com>
2022-03-15 17:50:21 +0100

    @dump($posts)

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                        Welcome {{Auth::user()->name}},
                     {{ __('You are logged in!') }}
                 </div>
             </div>
